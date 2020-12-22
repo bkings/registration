6d408a80e45e89a0a6984fceb1e94f5be9b9605d
bkings <bbikings@yahoo.com>
2020-12-22 23:48:21 +0545

    push

---
+++ src/containers/Setup/Fiscalyear/FiscalYear.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class FiscalYear extends Component {
+
+    state = {
+        yearCode,
+        year,
+        startDate,
+        endDate,
+        status
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default FiscalYear;
